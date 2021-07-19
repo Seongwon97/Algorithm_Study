e444b267359029e5195004381f69e14723e4747f
Seongwon97 <osw970325@naver.com>
2021-07-19 22:12:35 +0900

    Create Level2_문자열 압축[2020 카카오 블라인드 채용].py

---
+++ 프로그래머스/Level2_문자열 압축[2020 카카오 블라인드 채용].py
@@ -0,0 +1,27 @@
+def solution(s):
+    answer = len(s)
+    final_leng = 0
+    for leng in range(1, int(len(s)/2)+1):
+        result =''
+        start = 0
+        
+        while start < (len(s)):
+            move = leng
+            count = 1
+            while s[start:(start+leng)] == s[(start+move):(start+move+leng)]:
+                move += leng
+                count += 1
+            
+            if count > 1: 
+                result += str(count) + s[start:(start+leng)] 
+            elif len(s[start:]) < leng:
+                result += s[start: ] 
+            else: 
+                result += s[start:(start+leng)] 
+                
+            start += move
+                    
+        if len(result) < answer:
+            answer = len(result)
+
+    return answer

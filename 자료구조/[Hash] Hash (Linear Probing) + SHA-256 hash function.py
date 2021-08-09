0481884006633b58c4841ed9959bd029822c1765
Seongwon97 <57744251+Seongwon97@users.noreply.github.com>
2021-08-09 17:38:12 +0900

    Create [Hash] Hash (Linear Probing) + SHA-256 hash function.py

---
+++ 자료구조/[Hash] Hash (Linear Probing) + SHA-256 hash function.py
@@ -0,0 +1,39 @@
+import hashlib
+
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+        hash_object = hashlib.sha256()
+        hash_object.update(data.encode())
+        hex_dig = hash_object.hexdigest()
+        return int(hex_dig, 16) # 16진수로된 key값을 10진수인 정수로 변경
+
+def hash_function(key):
+    return key % 8
+
+def save_data(data, value):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                hash_table[index] = [index_key, value]
+                return
+            elif hash_table[index][0] == index_key:
+                hash_table[index][1] = value
+                return
+    else:
+        hash_table[hash_address] = [index_key, value]
+
+def read_data(data):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                return None
+            elif hash_table[index][0] == index_key:
+                return hash_table[index][1]
+    else:
+        return None

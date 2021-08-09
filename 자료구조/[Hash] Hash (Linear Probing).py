2a9cb43c7a744614c2418461a9e46a6ec4818600
Seongwon97 <57744251+Seongwon97@users.noreply.github.com>
2021-08-09 17:21:06 +0900

    Create [Hash] Hash (Linear Probing).py

---
+++ 자료구조/[Hash] Hash (Linear Probing).py
@@ -0,0 +1,38 @@
+# Linear Probing
+# Closing Hashing기법: Hash table안에있는 빈 공간에 데이터를 저장하는 방법
+# Hash address에 데이터에 저장이 되어있으면 hash table을 순회하면서 빈 공간에 저장하는 기법
+
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+    return hash(data)
+
+def hash_function(key):
+    return key % 8
+
+def save_data(data, value):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                hash_table[index] = [index_key, value]
+                return
+            elif hash_table[index][0] == index_key:
+                hash_table[index][1] = value
+                return
+    else:
+        hash_table[hash_address] = [index_key, value]
+
+def read_data(data):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                return None
+            elif hash_table[index][0] == index_key:
+                return hash_table[index][1]
+    else:
+        return None

91f2a00e758d1a53e0f482dc5dd2baa55b666c5f
Seongwon97 <osw970325@naver.com>
2021-08-30 23:40:19 +0900

    Create MergeSort.java

---
+++ 알고리즘/MergeSort.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import java.util.List;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static List<Integer> split(List<Integer> list){
+		
+	}
+
+}

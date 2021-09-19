de0cd35df686159d7cb260c46abc3f53740f77bc
Seongwon97 <osw970325@naver.com>
2021-09-19 23:47:45 +0900

    Create Dijkstra's algorithm(Python).py

---
+++ 알고리즘/Dijkstra's algorithm(Python).py
@@ -0,0 +1,23 @@
+import heapq
+
+def dijkstra(graph, start):
+    
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+    
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        
+        if distances[current_node] < current_distance:
+            continue
+            
+        for adjacent, weight in graph[current_node].items():
+            distance = current_distance + weight
+            
+            if distance < distances[adjacent]:
+                distances[adjacent] = distance
+                heapq.heappush(queue, [distance, adjacent])
+                
+    return distances

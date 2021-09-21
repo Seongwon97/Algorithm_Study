2fec76476ce326b7a794e7ed0de7dd0c9cac413b
Seongwon97 <osw970325@naver.com>
2021-09-21 23:40:14 +0900

    Create Kruskal's algorithm(Python).py

---
+++ 알고리즘/Kruskal's algorithm(Python).py
@@ -0,0 +1,47 @@
+parent = dict() # 부모 node에 대한 값을 저장
+rank = dict() # 각각의 node의 rank번호
+
+
+def find(node):
+    # path compression 기법
+    if parent[node] != node:
+        parent[node] = find(parent[node])
+    return parent[node]
+
+
+def union(node_v, node_u):
+    root1 = find(node_v)
+    root2 = find(node_u)
+    
+    # union-by-rank 기법
+    if rank[root1] > rank[root2]:
+        parent[root2] = root1
+    else:
+        parent[root1] = root2
+        if rank[root1] == rank[root2]:
+            rank[root2] += 1
+    
+    
+def make_set(node):
+    parent[node] = node
+    rank[node] = 0
+
+def kruskal(graph):
+    mst = list() # mst에는 Cycle이 없는 집합이 들어간다.
+    
+    # 1. 초기화
+    for node in graph['vertices']:
+        make_set(node)
+    
+    # 2. 간선 weight 기반 sorting
+    edges = graph['edges']
+    edges.sort()
+    
+    # 3. 간선 연결 (사이클 없는)
+    for edge in edges:
+        weight, node_v, node_u = edge
+        if find(node_v) != find(node_u):
+            union(node_v, node_u)
+            mst.append(edge)
+    
+    return mst

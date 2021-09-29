52bea0c0189a6016544714e85f8f5363a0cc408f
Seongwon97 <osw970325@naver.com>
2021-09-30 01:06:51 +0900

    Update B1966.java

--- 백준/B1966.java
+++ 백준/B1966.java
@@ -1,6 +1,7 @@
 package baekjoon;
 
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class B1966 {
@@ -13,20 +14,54 @@ public class B1966 {
 		int testCaseNum = sc.nextInt();
 		
 		
-		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		ArrayList<Integer> queue = new ArrayList<>();
 		for (int i=0; i<testCaseNum; i++) {
+			int count = 0;
 			queue.clear();
 			int inputNum = sc.nextInt();
-			int outputPosition = sc.nextInt();
+			int position = sc.nextInt();
 			
 			for(int j=0; j< inputNum; j++) {
 				queue.add(sc.nextInt());
 			}
 			
-			for(int j=0; j< outputPosition-1; j++) {
-				queue.poll();
+			int len = queue.size();
+			
+			
+			while(queue.size() != 0) {
+
+				
+				int max = Collections.max(queue);
+				
+//				for (int n: queue) {
+//					System.out.println(n);
+//				}
+				int removeNum = queue.remove(0);
+				
+//				System.out.println();
+//				for (int n: queue) {
+//					System.out.println(n);
+//				}
+				if (removeNum != max) {
+					queue.add(removeNum);
+					if (position == 0) position = queue.size()-1;
+					else position--;
+				}
+				else {
+					count++;
+					if (position == 0) {
+						System.out.println("count: "+count);
+						break;
+					}
+					position--;
+				}
+				
+//				System.out.println();
+//				for (int n: queue) {
+//					System.out.println(n);
+//				}
+				
 			}
-			System.out.println(queue.poll());
 		}
 		
 	}

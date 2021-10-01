602545850d2bdca3c64310687b5f189c4024911a
Seongwon97 <osw970325@naver.com>
2021-10-02 01:40:24 +0900

    Update B11650.java
    BufferedReader, StringTokenizer사용

--- 백준/B11650.java
+++ 백준/B11650.java
@@ -1,8 +1,12 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Point implements Comparable<Point>{
 	int x;
@@ -26,18 +30,18 @@ class Point implements Comparable<Point>{
 
 public class B11650 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int num = sc.nextInt();
+		int num = Integer.parseInt(br.readLine());
 		int x,y;
 		
 		ArrayList<Point> list = new ArrayList<>();
 		
 		for (int i=0; i<num; i++) {
-			x = sc.nextInt();
-			y = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
 			
 			list.add(new Point(x,y));
 		}

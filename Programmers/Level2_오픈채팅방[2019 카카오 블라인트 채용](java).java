2982ead2d470e79579d5b83b9eea885982412138
Seongwon97 <osw970325@naver.com>
2021-11-03 23:23:27 +0900

    Update Level2_오픈채팅방[2019 카카오 블라인트 채용](java).java

--- Programmers/Level2_오픈채팅방[2019 카카오 블라인트 채용](java).java
+++ Programmers/Level2_오픈채팅방[2019 카카오 블라인트 채용](java).java
@@ -1,101 +1,50 @@
+import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 class Solution {
     public String[] solution(String[] record) {
-                
-        User[] userList = new User[record.length];
-        int count = 0;
-        int changeCount = 0;
-        for (String message: record){
-            String[] strArr = message.split(" ");
-            // Enter인데 현재 없는 user면 user객체생성
-            if (strArr[0].equals("Enter") && !isExist(userList, strArr[1], count)){
-                userList[count] = new User(strArr[1], strArr[2]);
-                count++;
-            }
-            // Enter인데 user의 이름이 바껴서 접속한 경우
-            else if(strArr[0].equals("Enter") && isExist(userList, strArr[1], count)){
-                int userPosition = findUser(userList, strArr[1], count);
-                if(userList[userPosition].getName() != strArr[2]){
-                    userList[userPosition].setName(strArr[2]);   
-                }
-            }
-            // 현재 존재하는 User이고 enter이면 이름이 같은지 확인 후 이름이다르면 변경
-            else if (strArr[0].equals("Change")){
-                int userPosition = findUser(userList, strArr[1], count);
-                changeCount++;
-                if(userList[userPosition].getName() != strArr[2]){
-                    userList[userPosition].setName(strArr[2]);   
-                }
-            }
+        HashMap<String, String> userInfo = new HashMap<>();
+        int numOfAnswer = 0;
+        
+        for (String r : record) {
+            StringTokenizer st = new StringTokenizer(r);
+            String order = st.nextToken();
             
-        }
-        
-        
-        String[] answer = new String[record.length-changeCount];
-        // String array의 크기는 length를 사용, length()가 아님
-        int temp = 0;
-        for (String message: record){
-            String[] strArr = message.split(" ");
-            if (strArr[0].equals("Enter")){          
-                int userPosition = findUser(userList, strArr[1], count);
-                //System.out.println(userList[userPosition].getName()+"님이 들어왔습니다.");
-                answer[temp] = userList[userPosition].getName()+"님이 들어왔습니다.";
-                temp++;
-            }
-            else if (strArr[0].equals("Leave")){          
-                int userPosition = findUser(userList, strArr[1], count);
-                //System.out.println(userList[userPosition].getName()+"님이 나갔습니다.");
-                answer[temp] = userList[userPosition].getName()+"님이 나갔습니다.";
-                temp++;
+            if (order.charAt(0) == 'E' || order.charAt(0) == 'C') {
+                String id = st.nextToken();
+            	String name = st.nextToken();
+                userInfo.put(id, name);
+                numOfAnswer++;
             }
         }
         
+        ArrayList<String> result = new ArrayList<>();
+        int idx = 0;
+        for (String r : record) {
+            StringBuilder sb = new StringBuilder();
+            StringTokenizer st = new StringTokenizer(r);
+            String order = st.nextToken();
+            String id = st.nextToken();
+            
+            
+            if (order.charAt(0) == 'E') {
+                sb.append(userInfo.get(id));
+                sb.append("님이 들어왔습니다.");
+                result.add(sb.toString());
+            }
+            else if (order.charAt(0) == 'C') {
+                continue;
+            }
+            else if (order.charAt(0) == 'L') {
+                sb.append(userInfo.get(id));
+                sb.append("님이 나갔습니다.");
+                result.add(sb.toString());
+            }
+        }
+        
+        String[] answer = new String[result.size()];
+        result.toArray(answer);
         return answer;
     }
-    
-    public int findUser(User[] userList, String userId, int count){
-        for (int i=0; i< count; i++){
-            if(userList[i].getUserId().equals(userId))
-                return i;
-        }
-        return -1;
-    }
-                
-    // userId가 리스트에 존재하는지 확인
-    public boolean isExist(User[] userList, String userId, int count){
-        if (userList[0] != null) {
-            for (int i=0; i<count; i++){
-                if (userId.equals(userList[i].getUserId())){
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-}
-
-// 유저 정보를 보관하는 class
-class User {
-    private String userId;
-    private String name; 
-    
-    public User(){}
-    
-    public User(String userId, String name){
-        this.userId = userId;
-        this.name = name;
-    }
-    
-    public String getUserId(){
-        return userId;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
 }

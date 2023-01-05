0a5c84c4473b82bd4422d582ea0e0b7e9c24c447
Seongwon97 <osw970325@naver.com>
2023-01-06 00:13:24 +0900

    study: 이코테 ch12 구현 Q16 연구소

---
+++ 이코테/Ch13_dfs_bfs_Q16.java
@@ -0,0 +1,133 @@
+package chapter3_greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 연구소 (BFS)
+// https://www.acmicpc.net/problem/14502
+public class Ch13_dfs_bfs_Q16 {
+
+    private static int answer = 0;
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int[][] matrix = new int[N][M];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    matrix[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            dfs(matrix, 0);
+            System.out.println(answer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void dfs(int[][] matrix, int depth) {
+        if (depth == 3) {
+            int numOfSavePoint = findSavePoint(matrix);
+            answer = Math.max(answer, numOfSavePoint);
+            return;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][j] = 1;
+                    dfs(matrix, depth + 1);
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    private static int findSavePoint(int[][] matrix) {
+        int[][] temp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[0].length; j++) {
+                temp[i][j] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[0].length; j++) {
+                if (temp[i][j] == 2) {
+                    spreadVirus(temp, i, j);
+                }
+            }
+        }
+
+        int numOfSavePoints = 0;
+        for (int i = 0; i < temp.length; i++) {
+            for (int j = 0; j < temp[0].length; j++) {
+                if (temp[i][j] == 0) {
+                    numOfSavePoints++;
+                }
+            }
+        }
+
+        return numOfSavePoints;
+    }
+
+    private static void spreadVirus(int[][] matrix, int x, int y) {
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= matrix.length || ny < 0 || ny >= matrix[0].length || matrix[nx][ny] == 1) {
+                continue;
+            }
+            if (matrix[nx][ny] == 0) {
+                matrix[nx][ny] = 2;
+                spreadVirus(matrix, nx, ny);
+            }
+        }
+    }
+}
+/*
+Input
+7 7
+2 0 0 0 1 1 0
+0 0 1 0 1 2 0
+0 1 1 0 1 0 0
+0 1 0 0 0 0 0
+0 0 0 0 0 1 1
+0 1 0 0 0 0 0
+0 1 0 0 0 0 0
+
+Output
+27
+---
+Input
+4 6
+0 0 0 0 0 0
+1 0 0 0 0 2
+1 1 1 0 0 2
+0 0 0 0 0 2
+
+Output
+9
+---
+Input
+8 8
+2 0 0 0 0 0 0 2
+2 0 0 0 0 0 0 2
+2 0 0 0 0 0 0 2
+2 0 0 0 0 0 0 2
+2 0 0 0 0 0 0 2
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+
+Output
+3
+ */

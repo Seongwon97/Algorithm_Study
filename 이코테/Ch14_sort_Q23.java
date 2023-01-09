281c420b23238198c5e9ab99e7b6771742630254
Seongwon97 <osw970325@naver.com>
2023-01-09 22:48:03 +0900

    study: 이코테 ch13 sort Q23 국영수

---
+++ 이코테/Ch14_sort_Q23.java
@@ -0,0 +1,101 @@
+package chapter3_greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 국영수
+// https://www.acmicpc.net/problem/10825
+public class Ch14_sort_Q23 {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            List<Student> students = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                String name = st.nextToken();
+                int koreanScore = Integer.parseInt(st.nextToken());
+                int englishScore = Integer.parseInt(st.nextToken());
+                int mathScore = Integer.parseInt(st.nextToken());
+                students.add(new Student(name, koreanScore, englishScore, mathScore));
+            }
+
+            Collections.sort(students);
+
+            StringBuilder sb = new StringBuilder();
+            for (Student student : students) {
+                sb.append(student.name)
+                        .append("\n");
+            }
+
+            System.out.println(sb);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class Student implements Comparable<Student> {
+        String name;
+        int koreanScore;
+        int englishScore;
+        int mathScore;
+
+        public Student(String name, int koreanScore, int englishScore, int mathScore) {
+            this.name = name;
+            this.koreanScore = koreanScore;
+            this.englishScore = englishScore;
+            this.mathScore = mathScore;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            if (this.koreanScore != o.koreanScore) {
+                return o.koreanScore - this.koreanScore;
+            }
+            if (this.englishScore != o.englishScore) {
+                return this.englishScore - o.englishScore;
+            }
+            if (this.mathScore != o.mathScore) {
+                return o.mathScore - this.mathScore;
+            }
+            return this.name.compareTo(o.name);
+        }
+    }
+}
+
+/*
+Input
+12
+Junkyu 50 60 100
+Sangkeun 80 60 50
+Sunyoung 80 70 100
+Soong 50 60 90
+Haebin 50 60 100
+Kangsoo 60 80 100
+Donghyuk 80 60 100
+Sei 70 70 70
+Wonseob 70 70 90
+Sanghyun 70 70 80
+nsj 80 80 80
+Taewhan 50 60 90
+
+Output
+Donghyuk
+Sangkeun
+Sunyoung
+nsj
+Wonseob
+Sanghyun
+Sei
+Kangsoo
+Haebin
+Junkyu
+Soong
+Taewhan
+ */

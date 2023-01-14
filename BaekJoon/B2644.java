7dd8605a629b0d558a3e4170b15d32d8ef608b32
Seongwon97 <osw970325@naver.com>
2023-01-14 15:58:10 +0900

    study: 백준 2644 촌수계신 #BFS/DFS
    풀이는 DFS

--- BaekJoon/B2644.java
+++ BaekJoon/B2644.java
@@ -1,53 +1,85 @@
-package baekjoon;
+package BaekJoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
- 
+
+// 촌수계산
+// https://www.acmicpc.net/problem/2644
 public class B2644 {
- 
-    static int[][] map;
-    static int n, p1, p2, par_ch;
-    static int[] check;
- 
-    static void BFS(int start) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(start);
-        while (!q.isEmpty()) {
-            int x = q.poll();
-            for (int i = 1; i < map.length; i++) {
-                if (map[x][i] == 1 && check[i] == 0) {
-                    q.offer(i);
-                    check[i] = check[x] + 1; // check에 저장.
-                }
+
+    static int answer = -1;
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int target = Integer.parseInt(st.nextToken());
+
+            boolean[][] matrix = new boolean[n + 1][n + 1];
+
+            int m = Integer.parseInt(br.readLine());
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                int people1 = Integer.parseInt(st.nextToken());
+                int people2 = Integer.parseInt(st.nextToken());
+                matrix[people1][people2] = true;
+                matrix[people2][people1] = true;
+            }
+
+            boolean[] visited = new boolean[n + 1];
+             dfs(0, matrix, visited, start, target);
+
+            System.out.println(answer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void dfs(int depth, boolean[][] matrix, boolean[] visited, int start, int target) {
+        if (start == target) {
+            answer = depth;
+        }
+
+        visited[start] = true;
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[start][i] && !visited[i]) {
+                dfs(depth + 1, matrix, visited, i, target);
             }
         }
-        System.out.println(check[p2] == 0 ? -1 : check[p2]);
     }
- 
-    public static void main(String[] args) throws IOException {
- 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine()); // 사람 수
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        p1 = Integer.parseInt(st.nextToken()); // 비교대상 1
-        p2 = Integer.parseInt(st.nextToken()); // 비교대상 2
-        par_ch = Integer.parseInt(br.readLine()); // 부모 자식들 간의 관계의 개수
-        map = new int[n + 1][n + 1]; // index 1 부터
- 
-        for (int i = 0; i < par_ch; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            map[a][b] = map[b][a] = 1;
-        }
-        check = new int[n + 1];
- 
-        BFS(p1);
- 
-    }
- 
 }
+
+/*
+Input
+9
+7 3
+7
+1 2
+1 3
+2 7
+2 8
+2 9
+4 5
+4 6
+
+Output
+3
+---
+Input
+9
+8 6
+7
+1 2
+1 3
+2 7
+2 8
+2 9
+4 5
+4 6
+
+Output
+-1
+*/

020faa058dec917c7b52efec4e4deb51d39f80b2
Seongwon97 <osw970325@naver.com>
2023-03-31 22:23:37 +0900

    study: 프로그래머스 Level2 네트워크

--- Programmers/Level2_네트워크_DFS (java).java
+++ Programmers/Level2_네트워크_DFS (java).java
@@ -1,26 +1,24 @@
-class Solution { 
+class Solution {
     public int solution(int n, int[][] computers) {
-        int answer=0;
+        int answer = 0;
         boolean[] visited = new boolean[n];
-        for (int i=0;i<n;i++){
-            answer += dfs(i,computers, visited);
+        for (int i = 0; i < n; i++) {
+            answer += dfs(i, computers, visited);
         }
-        return answer; 
-    } 
-    
-    
-    public static int dfs(int node, int[][] computers, boolean[] visited){
-        if (visited[node])
-            return 0;
-        
-        visited[node] = true;
-        for (int i=0; i<computers.length; i++){
-            if (computers[node][i] == 1){
-                dfs(i, computers, visited); 
-            }
-        }
-        
-        return 1;
+        return answer;
     }
 
+    public int dfs(int node, int[][] computers, boolean[] visited) {
+        if (visited[node]) {
+            return 0;
+        }
+
+        visited[node] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[node][i] == 1) {
+                dfs(i, computers, visited);
+            }
+        }
+        return 1;
+    }
 }

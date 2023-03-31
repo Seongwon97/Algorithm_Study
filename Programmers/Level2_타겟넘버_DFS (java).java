9f8d935f584bf970f9eb8d5763474a9c85d14985
Seongwon97 <osw970325@naver.com>
2023-03-31 21:47:46 +0900

    study: 프로그래머스 Level2 타겟넘버

--- Programmers/Level2_타겟넘버_DFS (java).java
+++ Programmers/Level2_타겟넘버_DFS (java).java
@@ -2,29 +2,30 @@ import java.util.*;
 
 class Solution {
     static int count = 0;
+
     public int solution(int[] numbers, int target) {
         dfs(numbers, 0, 0, target);
-        
+
         return count;
     }
-    
+
     public static void dfs(int[] numbers, int currentRow, int sum, int target) {
-        if (currentRow == numbers.length){
-            if(sum == target)
+        if (currentRow == numbers.length) {
+            if (sum == target) {
                 count++;
+            }
+            return;
         }
-        else {
-            for (int i = 0; i < 2; i++){
-                if(i==0) {
-                    sum += numbers[currentRow];
-                    dfs(numbers, currentRow+1, sum, target);
-                    sum -= numbers[currentRow];
-                } 
-                else {
-                    sum -= numbers[currentRow];
-                    dfs(numbers, currentRow+1, sum, target);
-                    sum += numbers[currentRow];
-                }
+
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                sum += numbers[currentRow];
+                dfs(numbers, currentRow + 1, sum, target);
+                sum -= numbers[currentRow];
+            } else {
+                sum -= numbers[currentRow];
+                dfs(numbers, currentRow + 1, sum, target);
+                sum += numbers[currentRow];
             }
         }
     }
